d3bb4b5f3c25279c7e1ea62b14e7710d3d90b808
nevans <nevans>
2007-12-02 06:32:15 +0000

    DB

---
+++ db/refresh_dump.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+tables=( jump_page staff_permission staff_group )
+
+if [ "$1" = '' ]
+then
+    echo "o hi, pass the database password as an argument plx. thx.";
+    exit 2;
+fi
+
+mysqldump -d -u root --password=$1 kkk > mysql5_ddl.sql
+
+for table in  ${tables[@]}
+do
+    mysqldump -t -c -u root --password=$1 kkk $table > data/${table}.sql
+done
